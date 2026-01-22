98c9c27314f108add3da96bcbe8809847e38cdd8
shailendrasinghofficial3 <shailendrasinghofficial3@gmail.com>
2026-01-23 00:57:47 +0530

    chore:added celery flower beat start.sh script"

---
+++ backend/docker/local/celery/beat/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+
+set -o nounset
+
+set -o pipefail
+
+exec watchfiles --filter python celery.__main__.main --args '-A backend.app.core.celery_app beat -l INFO'
---
+++ backend/docker/local/celery/flower/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+
+set -o nounset
+
+set -o pipefail
+
+FLOWER_CMD="celery \
+-A backend.app.core.celery_App \
+-b ${CELERY_BROKER_URL} \
+flower \ 
+--address=0.0.0.0 \
+--port=5555 \
+--basic_auth=${CELERY_FLOWER_USER}:${CELERY_FLOWER_PASSWORD}"
+
+
+exec watchfiles \
+--filter python \
+--ignore_paths '.vens,.git,__pycache__,*.pyc' \
+"${FLOWER_CMD}"
